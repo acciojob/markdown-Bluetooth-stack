8425d3eac0ae5baeffaa0203d3440e9ad77ffd9c
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-24 01:06:04 +0530

    again

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from "react";
+// import marked from 'marked'
 
 const Markdown = () => {
-    let [render, setRender] = useState(text);
-    let [text, setText] = useState('');
+    const [markdown, setMarkdown] = useState('');
 
     useEffect(() => {
-        let str = '';
-        text && (str = text.replace(/\n/g, " "));
-        let str1 = str.split(' ');
-        setRender(str1[1]);
-    }, [text])
+        // const renderedMarkdown = marked(markdown);
+        document.getElementById('preview').innerHTML = markdown;
+    }, [markdown]);
+
+    const handleMarkdownChange = (event) => {
+        setMarkdown(event.target.value);
+    };
 
     return (
         <div>
-            <div className="app">
-                <textarea rows={40} cols={80} onChange={(e) => { setText(e.target.value) }}></textarea>
+            <div>
+                <textarea rows={30} cols={40} value={markdown} onChange={handleMarkdownChange} />
             </div>
-            <div className="preview">
-                <h1>
-                    <strong>
-                        {render}
-
-                    </strong>
-                </h1>
+            <div>
+                <h1 id="preview"></h1>
             </div>
         </div>
-    )
+    );
 }
 
 export default Markdown;
 
+
+
+// let [render, setRender] = useState(text);
+// let [text, setText] = useState('');
+
+// useEffect(() => {
+//     let str = '';
+//     text && (str = text.replace(/\n/g, " "));
+//     let str1 = str.split(' ');
+//     setRender(str1[1]);
+// }, [text])
+
+// return (
+//     <div>
+//         <div className="app">
+//             <textarea rows={40} cols={80} onChange={(e) => { setText(e.target.value) }}></textarea>
+//         </div>
+//         <div className="preview">
+//             <h1>
+//                 <strong>
+//                     {render}
+//                 </strong>
+//             </h1>
+//         </div>
+//     </div>
+// )
