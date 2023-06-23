825bd23c3d327cf14b774c8ae3e0bfca0db25b36
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-24 01:21:47 +0530

    bold

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -4,14 +4,19 @@ import React, { useEffect, useState } from "react";
 const Markdown = () => {
     let [render, setRender] = useState(text);
     let [text, setText] = useState('');
-    
+
     useEffect(() => {
         let str = '';
         text && (str = text.replace(/\n/g, " "));
         let str1 = str.split(' ');
         setRender(str1[1]);
+        str1.map(bold => {
+            if (bold == 'bold') {
+                setRender(bold)
+            }
+        })
     }, [text])
-    
+
     return (
         <div>
             <div className="app">
@@ -25,7 +30,7 @@ const Markdown = () => {
                 </h1>
             </div>
         </div>
-    )   
+    )
 }
 
 export default Markdown;
